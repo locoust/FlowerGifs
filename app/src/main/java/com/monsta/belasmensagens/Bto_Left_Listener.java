328d1f008b9f2bd627eba576b0505050709cd7c5
Christian Lopez <chrpriv@hotmail.com>
2018-01-24 14:09:38 +0100

    first listener class

---
+++ app/src/main/java/com/monsta/belasmensagens/Bto_Left_Listener.java
@@ -0,0 +1,15 @@
+package com.monsta.belasmensagens;
+
+import android.view.View;
+
+/**
+ * Created by christianlopez on 24/1/18.
+ */
+public class Bto_Left_Listener implements View.OnClickListener
+{
+    @Override
+    public void onClick(View v)
+    {
+
+    }
+}
